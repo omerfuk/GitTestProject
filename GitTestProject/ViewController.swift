79ea2b9be5b2d7febadd725d241f421df63464bd
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-27 22:49:07 +0300

    Git2222 added

--- GitTestProject/ViewController.swift
+++ GitTestProject/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("First Commit")
+        print("Git2222")
     }
 
 
